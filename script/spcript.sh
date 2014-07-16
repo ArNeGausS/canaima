f156f20154efd19c434adcfc2fec633c7ef2873e
root <root@canaima-popular.(none)>
2014-07-16 14:26:09 -0430

    subiendo primer escript

---
+++ script/spcript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+fecha=$(date +"%m-%d-%Y-%T")
+
+echo "**************Respaldo************************************"
+cd /home/fundaciteapure/control/
+cp -r /home/fundaciteapure/control/canaima/ /home/fundaciteapure/recovery/recovery$fecha
+echo "*****************fin del Respaldo************************"
