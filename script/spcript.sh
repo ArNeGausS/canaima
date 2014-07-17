e9527fe4b802d3d6568358881c6c75ed0048dcc1
root <root@canaima-popular.(none)>
2014-07-17 15:26:44 -0430

    subiendo nuevo archivo

--- script/spcript.sh
+++ script/spcript.sh
@@ -1,7 +1,6 @@
 #!/bin/bash/
-fecha=$(date +"%m-%d-%Y-%T")
-
 echo "**************Respaldo************************************"
+fecha=$(date +"%m-%d-%Y-%T")
 cd /home/fundaciteapure/control/
 cp -r /home/fundaciteapure/control/canaima/ /home/fundaciteapure/recovery/ recovery$fecha
 echo "*****************fin del Respaldo************************"
